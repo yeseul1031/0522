b9762cd6a6f1a796c8d9fe637057db22c6149759
yeseul1031 <heoyeseul1031@gmail.com>
2025-05-22 18:07:30 +0900

    change : part3

--- part-3/01_basicChaining.js
+++ part-3/01_basicChaining.js
@@ -1,19 +1,14 @@
+
 const newsURL = 'http://localhost:4999/data/latestNews';
 const weatherURL = 'http://localhost:4999/data/weather';
 
 function getNewsAndWeather() {
   return fetch(newsURL)
-    .then(newsResponse => {
-      if (!newsResponse.ok) throw new Error('뉴스 데이터 가져오기 실패');
-      return newsResponse.json();
-    })
-    .then(newsData => {
+    .then((newsResponse) => newsResponse.json())
+    .then((newsData) => {
       return fetch(weatherURL)
-        .then(weatherResponse => {
-          if (!weatherResponse.ok) throw new Error('날씨 데이터 가져오기 실패');
-          return weatherResponse.json();
-        })
-        .then(weatherData => {
+        .then((weatherResponse) => weatherResponse.json())
+        .then((weatherData) => {
           return {
             news: newsData,
             weather: weatherData
--- part-3/02_promiseAll.js
+++ part-3/02_promiseAll.js
@@ -2,21 +2,13 @@ const newsURL = 'http://localhost:4999/data/latestNews';
 const weatherURL = 'http://localhost:4999/data/weather';
 
 function getNewsAndWeatherAll() {
+  
   return Promise.all([
-    fetch(newsURL).then(res => {
-      if (!res.ok) throw new Error('뉴스 데이터 가져오기 실패');
-      return res.json();
-    }),
-    fetch(weatherURL).then(res => {
-      if (!res.ok) throw new Error('날씨 데이터 가져오기 실패');
-      return res.json();
-    })
+    fetch(newsURL).then((res) => res.json()),
+    fetch(weatherURL).then((res) => res.json())
   ])
-  .then(([newsData, weatherData]) => {
-    return {
-      news: newsData,
-      weather: weatherData
-    };
+  .then(([news, weather]) => {
+    return { news, weather };
   });
 }
 
